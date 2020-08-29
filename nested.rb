8f45226aab7bb022fe3babe15efb777dda7719b2
selam lemma <oneiconforlife26@gmail.com>
2020-08-29 13:32:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,8 +41,7 @@ def adding_matz
 	programmer_hash =
      {:grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"]},
      :alan_kay => {:known_for => "Object Orientation",:languages => ["Smalltalk", "LISP"]},
-     :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]
-        }
+     :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]}
      }
       programmer_hash[:yukihiro_matsumoto]=>{}
 end
