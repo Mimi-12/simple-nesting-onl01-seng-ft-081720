e4d403ea1bfd13345d64e597c1d832a8c47c6003
selam lemma <oneiconforlife26@gmail.com>
2020-08-29 13:33:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -44,6 +44,8 @@ def adding_matz
      :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]}
      }
       programmer_hash[:yukihiro_matsumoto]=>{}
+      programmer_hash[:yukihiro_matsumoto]=>{:known_for => "Ruby",:languages => ["LISP", "C"]}
+
 end
 
 def changing_alan
