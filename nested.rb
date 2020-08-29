44dcf34798756b98463e2d79a173e6d1d8c62385
selam lemma <oneiconforlife26@gmail.com>
2020-08-29 13:41:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -43,8 +43,8 @@ def adding_matz
      :alan_kay => {:known_for => "Object Orientation",:languages => ["Smalltalk", "LISP"]},
      :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]}
      }
-      programmer_hash[:yukihiro_matsumoto]=>{}
-      programmer_hash[:yukihiro_matsumoto]=>{:known_for =>"Ruby"}
+      programmer_hash[:yukihiro_matsumoto]=>{},
+      programmer_hash[:yukihiro_matsumoto]=>{:known_for =>"Ruby"},
       programmer_hash[:yukihiro_matsumoto]=>{:languages => ["LISP", "C"]}
 end
 
