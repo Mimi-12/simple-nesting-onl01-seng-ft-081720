2428eb490ebbc9228d27056b2f04d7e51c768d5e
selam lemma <oneiconforlife26@gmail.com>
2020-08-29 12:16:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -49,26 +49,17 @@ end
 
 def changing_alan
 	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
+ 		{:grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"]},
+     :alan_kay => {:known_for => "Object Orientation",:languages => ["Smalltalk", "LISP"]},
+     :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]
         }
      }
      programmer_hash[:alan_kay][:known_for]="GUI"
+     
      programmer_hash[:alan_kay][:known_for]
      #alans_new_info = "GUI"
-
-
 end
+
 
 def adding_to_dennis
 	programmer_hash =
